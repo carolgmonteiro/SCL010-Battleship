590397c322c456e49db03d721e080df6ccaade62
carolgmonteiro <carolgmonteiro@live.com>
2019-12-06 12:49:14 -0300

    router de homepage y Gameboard

--- game/src/App.js
+++ game/src/App.js
@@ -1,31 +1,22 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import "./bootstrap.min.css";
 import { Col, Row, Container } from "reactstrap";
-// import Home from "../src/components/Views/Home";
-
-// import Home from "./components/Views/Home";
-// import { Container, Row, Col } from "reactstrap";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from "../src/components/Views/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GameBoard from "../src/components/Views/GameBoard";
 
-function App() {
-  return (
-    <main className="App">
-      <Container>
-        {/* <Home /> */}
-        <GameBoard />
-        {/* <Router>
-        <Switch>
-          <Route exact path="/" component={GameBoard} />
-          <Route exact path="/" component={Home} />
-          <Route path="/GameBoard" component={GameBoard} />
-        </Switch>
-      </Router> */}
-      </Container>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Route exact path="/" component={Home} />
+        <main className="App">
+          <Route path="/gameboard" component={GameBoard} />
+        </main>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
